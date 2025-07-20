47f9da9d169333cce25b39d6e8781dab82844828
tristanjakobi <tristanjakobi08@gmail.com>
2025-07-20 15:18:02 +0200

    Update MusicControlModule to register receiver with exported flag for Android 12 and above

--- android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
+++ android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
@@ -201,8 +201,12 @@ public class MusicControlModule extends ReactContextBaseJavaModule implements Co
         filter.addAction(Intent.ACTION_MEDIA_BUTTON);
         filter.addAction(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
         receiver = new MusicControlReceiver(this, context);
-        context.registerReceiver(receiver, filter);
-
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
+        } else {
+            context.registerReceiver(receiver, filter);
+        }
         Intent myIntent = new Intent(context, MusicControlNotification.NotificationService.class);
 
         afListener = new MusicControlAudioFocusListener(context, emitter, volume);
